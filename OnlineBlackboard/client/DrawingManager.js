2c176da7367ac4f550029ef15f4d3f6242cc23ff
saralalic <a.lalic@hotmail.com>
2016-12-05 02:33:26 +0100

    Refaktoring - Dalje prebacivanje dela funkcionalnosti iz DrawingManager u Figure

--- OnlineBlackboard/client/DrawingManager.js
+++ OnlineBlackboard/client/DrawingManager.js
@@ -280,142 +280,14 @@ export class DrawingManager
 		{
 			var figureToSave;
 			isDown = false;
-			var drawingMode = Session.get('DrawingMode');
 			if(selectedFigureForEditing == null)
 			{
 				var pointer = canvas.getPointer(o.e);
-				var strokeColor = Session.get('SelectedStrokeColorWithAlpha');
-				var fillColor = Session.get('SelectedFillColorWithAlpha');
-				var selectedStrokeWidth = Session.get('SelectedStrokeWidth');
-				switch(drawingMode)
-				{
-					case FiguresEnum.RectFigure:
-					{
-						var leftRect = Math.min(origX,pointer.x);
-						var topRect = Math.min(origY,pointer.y);
-						var widthRect = Math.abs(origX - pointer.x);
-						var heightRect = Math.abs(origY - pointer.y);
-						figureToSave = new Rect(0, 1, 1, selectedStrokeWidth, strokeColor, fillColor, topRect, leftRect, widthRect, heightRect);
-						break;
-					}
-					case FiguresEnum.CircleFigure:
-					{
-						var circleRadius = Math.sqrt(Math.pow(origX - pointer.x, 2) + Math.pow(origY - pointer.y, 2));
-						figureToSave = new Circle(0, 1, 1, selectedStrokeWidth, strokeColor, fillColor, origY, origX, circleRadius);
-						break;
-					}
-					case FiguresEnum.TriangleFigure:
-					{
-						var leftTriangle = Math.min(origX,pointer.x);
-						var topTriangle = Math.min(origY,pointer.y);
-						var widthTriangle = Math.abs(origX - pointer.x);
-						var heightTriangle = Math.abs(origY - pointer.y);
-						figureToSave = new Triangle(0, 1, 1, selectedStrokeWidth, strokeColor, fillColor, topTriangle, leftTriangle, widthTriangle, heightTriangle);
-						break;
-					}
-					case FiguresEnum.EllipseFigure:	
-					{
-						var xRadius = Math.abs(origX - pointer.x);
-						var yRadius = Math.abs(origY - pointer.y);
-						figureToSave = new Ellipse(0, 1, 1, selectedStrokeWidth, strokeColor, fillColor, origY, origX, xRadius, yRadius);
-						break;
-					}
-					case FiguresEnum.SquareFigure:
-					{
-						var leftSquare= Math.min(origX,pointer.x);
-						var topSquare = Math.min(origY,pointer.y);
-						var widthSquare = Math.min(Math.abs(origX - pointer.x), Math.abs(origY - pointer.y));
-						figureToSave = new Square(0, 1, 1, selectedStrokeWidth, strokeColor, fillColor, topSquare, leftSquare, widthSquare);
-						break;
-					}
-					case FiguresEnum.PolygonFigure:	
-					{
-						var polygonRadius = Math.sqrt(Math.pow(origX - pointer.x, 2) + Math.pow(origY - pointer.y, 2));
-						var leftPolygon = origX-polygonRadius;
-						var	topPolygon = origY-polygonRadius;
-						figureToSave = new Polygon(0, 1, 1, selectedStrokeWidth, strokeColor, fillColor, topPolygon, leftPolygon, 8, polygonRadius);										
-						break;
-					}
-					case FiguresEnum.TextFigure:
-					{
-						var pointer = canvas.getPointer(o.e);
-						figureToSave = new Text(0, 1, 1, selectedStrokeWidth, strokeColor, fillColor, pointer.y, pointer.x, 400, 400, 'Hello world');										
-						break;
-					}
-				}
+				Figure.saveDrawingFigureInDb(drawingFigure, pointer, origX, origY)
 			}
 			else
 			{
-				switch(selectedFigureForEditing.target.figureType)
-				{
-					case FiguresEnum.RectFigure:
-					{
-						figureToEdit = new Rect(selectedFigureForEditing.target.angle, selectedFigureForEditing.target.scaleX, selectedFigureForEditing.target.scaleY, selectedFigureForEditing.target.strokeWidth, selectedFigureForEditing.target.stroke, selectedFigureForEditing.target.fill, selectedFigureForEditing.target.top, selectedFigureForEditing.target.left, selectedFigureForEditing.target.width, selectedFigureForEditing.target.height);
-						break;
-					}
-					case FiguresEnum.CircleFigure:
-					{
-						figureToEdit = new Circle(selectedFigureForEditing.target.angle, selectedFigureForEditing.target.scaleX, selectedFigureForEditing.target.scaleY, selectedFigureForEditing.target.strokeWidth, selectedFigureForEditing.target.stroke, selectedFigureForEditing.target.fill, selectedFigureForEditing.target.top, selectedFigureForEditing.target.left, selectedFigureForEditing.target.width * 0.5);
-						break;
-					}
-					case FiguresEnum.TriangleFigure:
-					{
-						figureToEdit = new Triangle(selectedFigureForEditing.target.angle, selectedFigureForEditing.target.scaleX, selectedFigureForEditing.target.scaleY, selectedFigureForEditing.target.strokeWidth, selectedFigureForEditing.target.stroke, selectedFigureForEditing.target.fill, selectedFigureForEditing.target.top, selectedFigureForEditing.target.left, selectedFigureForEditing.target.width, selectedFigureForEditing.target.height);
-						break;
-					}
-					case FiguresEnum.EllipseFigure:	
-					{
-						figureToEdit = new Ellipse(selectedFigureForEditing.target.angle, selectedFigureForEditing.target.scaleX, selectedFigureForEditing.target.scaleY, selectedFigureForEditing.target.strokeWidth, selectedFigureForEditing.target.stroke, selectedFigureForEditing.target.fill, selectedFigureForEditing.target.top, selectedFigureForEditing.target.left, selectedFigureForEditing.target.rx, selectedFigureForEditing.target.ry);
-						break;
-					}
-					case FiguresEnum.SquareFigure:
-					{
-						figureToEdit = new Square(selectedFigureForEditing.target.angle, selectedFigureForEditing.target.scaleX, selectedFigureForEditing.target.scaleY, selectedFigureForEditing.target.strokeWidth, selectedFigureForEditing.target.stroke, selectedFigureForEditing.target.fill, selectedFigureForEditing.target.top, selectedFigureForEditing.target.left, selectedFigureForEditing.target.width);
-						break;
-					}
-					case FiguresEnum.PolygonFigure:	
-					{
-						var polygonRadius = Math.sqrt(Math.pow(origX - pointer.x, 2) + Math.pow(origY - pointer.y, 2));
-						var leftPolygon = origX-polygonRadius;
-						var	topPolygon = origY-polygonRadius;
-						figureToEdit = new Polygon(selectedFigureForEditing.target.angle, selectedFigureForEditing.target.scaleX, selectedFigureForEditing.target.scaleY, selectedStrokeWidth, strokeColor, fillColor, topPolygon, leftPolygon, 8, polygonRadius);										
-						break;
-					}
-					case FiguresEnum.TextFigure:
-					{
-						var pointer = canvas.getPointer(o.e);
-						figureToEdit = new Text(selectedFigureForEditing.target.angle, selectedFigureForEditing.target.scaleX, selectedFigureForEditing.target.scaleY, selectedStrokeWidth, strokeColor, fillColor, pointer.y, pointer.x, 400, 400, 'Hello world');										
-						break;
-					}
-				}
-			}
-			if(drawingMode != FiguresEnum.EnableAll && drawingMode != FiguresEnum.DisableAll)
-			{
-				Meteor.call('saveFigureInDB', figureToSave, function (error, result) 
-					{
-						if (error) 
-						{
-							console.log(error);
-						}
-						else 
-						{
-							console.log('success');
-						}
-					});
-			}
-			else if(drawingMode == FiguresEnum.EnableAll && selectedFigureForEditing != null)
-			{
-				Meteor.call('updateFigureInDB', selectedFigureForEditing.target._id, figureToEdit, function (error) 
-				{
-					if (error) 
-					{
-						console.log(error);
-					}
-					else 
-					{
-						console.log('success');
-					}
-				});
+				Figure.updateExistingFigureInDB(selectedFigureForEditing,figureToEdit);
 			}
 		});
 	}
@@ -448,5 +320,3 @@ export class DrawingManager
 		figureToDraw.figureType = singleFigure.type;
 	}
 }
-
-//test
