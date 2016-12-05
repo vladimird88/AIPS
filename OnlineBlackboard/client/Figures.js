ad8c2c81d7377c912550cd8ef32ce594df3c244a
saralalic <a.lalic@hotmail.com>
2016-12-05 02:15:12 +0100

    Refaktoring koda (prebacivanje dela funkcionalnosti iz DrawingManager-a u Figures fajl)

--- OnlineBlackboard/client/Figures.js
+++ OnlineBlackboard/client/Figures.js
@@ -25,6 +25,153 @@ export class Figure {
 		this.time = Date.now();
     }
 	
+	static startDrawingFigure(pointer)
+	{
+		var drawingFigure;
+		var drawingMode = Session.get('DrawingMode');
+		var selectedStrokeWidth = Session.get('SelectedStrokeWidth');
+		var selectedStrokeColorWithAlpha = Session.get('SelectedStrokeColorWithAlpha');
+		var selectedFillColorWithAlpha = Session.get('SelectedFillColorWithAlpha');
+		switch(drawingMode)
+		{
+			case FiguresEnum.RectFigure:	
+			{
+				drawingFigure = new fabric.Rect({
+					left: pointer.x,
+					top: pointer.y,
+					width:1,
+					height:1,
+					strokeWidth: selectedStrokeWidth,
+					stroke: selectedStrokeColorWithAlpha,
+					selectable: false,
+					fill: selectedFillColorWithAlpha,
+					originX: 'left', originY: 'top'
+				});
+				canvas.add(drawingFigure);
+				break;
+			}
+			case FiguresEnum.CircleFigure:	
+			{
+				drawingFigure = new fabric.Circle({
+					left: pointer.x,
+					top: pointer.y,
+					radius: 1,
+					strokeWidth: selectedStrokeWidth,
+					stroke: selectedStrokeColorWithAlpha,
+					selectable: false,
+					fill: selectedFillColorWithAlpha,
+					originX: 'center', originY: 'center'
+				});
+				canvas.add(drawingFigure);
+				break;
+			}
+			case FiguresEnum.TriangleFigure:
+			{
+				drawingFigure = new fabric.Triangle({
+					left: pointer.x,
+					top: pointer.y,
+					width:1,
+					height:1,
+					strokeWidth: selectedStrokeWidth,
+					stroke: selectedStrokeColorWithAlpha,
+					selectable: false,
+					fill: selectedFillColorWithAlpha,
+					originX: 'left', originY: 'top'
+				});
+				canvas.add(drawingFigure);
+				break;
+			}
+			/*case FiguresEnum.LineFigure:			//line, spaja 2 tacke
+			{
+				if (point1 === undefined) 
+				{
+					point1 = new fabric.Point(origX, origY)
+				} 
+				else 
+				{
+					canvas.add(new fabric.Line([point1.x, point1.y, origX, origY], {
+						stroke: selectedStrokeColorWithAlpha,
+						hasControls: false,
+						strokeWidth: selectedStrokeWidth,
+						hasBorders: false,
+						lockMovementX: true,
+						lockMovementY: true,
+						hoverCursor: 'default'
+					}))
+					point1 = undefined;
+				}
+				break;
+			}*/
+			case FiguresEnum.EllipseFigure:
+			{
+				drawingFigure = new fabric.Ellipse({
+					left: pointer.x,
+					top: pointer.y,
+					rx: 1,
+					ry: 1,
+					strokeWidth: selectedStrokeWidth,
+					stroke: selectedStrokeColorWithAlpha,
+					selectable: false,
+					fill: selectedFillColorWithAlpha,
+					originX: 'center', originY: 'center'
+				});
+				canvas.add(drawingFigure);
+				break;
+			}
+			case FiguresEnum.SquareFigure:
+			{
+				drawingFigure = new fabric.Rect({
+					left: pointer.x,
+					top: pointer.y,
+					width:1,
+					height:1,
+					strokeWidth: selectedStrokeWidth,
+					stroke: selectedStrokeColorWithAlpha,
+					selectable: false,
+					fill: selectedFillColorWithAlpha,
+					originX: 'left', originY: 'top'
+				});
+				canvas.add(drawingFigure);
+				break;
+			}
+			case FiguresEnum.PolygonFigure:	
+			{
+				var points = DrawingManager.regularPolygonPoints(8,1);	
+				drawingFigure = new fabric.Polygon(
+					points, {
+						left: pointer.x,
+						top: pointer.y,
+						angle: 0,
+						fill: selectedFillColorWithAlpha,
+						stroke: selectedStrokeColorWithAlpha,
+						selectable: false,
+						originX: 'left', originY: 'top'
+					  }
+					);
+				canvas.add(drawingFigure);											
+				break;
+			}
+			case FiguresEnum.TextFigure:
+			{
+				drawingFigure = new fabric.IText('Hello world', {
+					left: pointer.x,
+					top: pointer.y,
+					width:400,
+					height:400,
+					strokeWidth: selectedStrokeWidth,
+					stroke: selectedStrokeColorWithAlpha,
+					selectable: false,
+					fill: selectedFillColorWithAlpha,
+					originX: 'left', originY: 'top'
+				});
+				canvas.add(drawingFigure);
+				//objectsList.push(square);
+				break;
+			}
+		}
+		return drawingFigure;
+	}
+	
 	//adapter / fabric
 	static createFigure(singleFigure)
 	{
@@ -166,6 +313,72 @@ export class Figure {
 			}
 		}
 		return figureToDraw;
+	}
+	
+	static updateDrawingFigure(drawingFigure, pointer, origX, origY)
+	{
+		var drawingMode = Session.get('DrawingMode');
+		switch(drawingMode)
+		{
+			case FiguresEnum.EnableAll:
+			{
+				break;
+			}
+			case FiguresEnum.DisableAll:
+			{
+				break;
+			}
+			case FiguresEnum.RectFigure:
+			{
+				var leftRect = Math.min(origX,pointer.x);
+				var topRect = Math.min(origY,pointer.y);
+				var widthRect = Math.abs(origX - pointer.x);
+				var heightRect = Math.abs(origY - pointer.y);
+				drawingFigure.set({ left: leftRect, top: topRect, width: widthRect, height: heightRect });
+				canvas.renderAll();
+				break;
+			}
+			case FiguresEnum.CircleFigure:
+			{
+				drawingFigure.set({ radius: Math.sqrt(Math.pow(origX - pointer.x, 2) + Math.pow(origY - pointer.y, 2)) });
+				break;
+			}
+			case FiguresEnum.TriangleFigure:
+			{
+				var leftTriangle = Math.min(origX,pointer.x);
+				var topTriangle = Math.min(origY,pointer.y);
+				var widthTriangle = Math.abs(origX - pointer.x);
+				var heightTriangle = Math.abs(origY - pointer.y);
+				drawingFigure.set({ left: leftTriangle, top: topTriangle, width: widthTriangle, height: heightTriangle });
+				break;
+			}
+			case FiguresEnum.EllipseFigure:
+			{
+				drawingFigure.set({ rx: Math.abs(origX - pointer.x), ry: Math.abs(origY - pointer.y)});
+				break;
+			}
+			case FiguresEnum.SquareFigure:
+			{
+				var widthSquare = Math.min(Math.abs(origX - pointer.x), Math.abs(origY - pointer.y));
+				var leftSquare = pointer.x > origX ? origX : Math.max(pointer.x,origX-widthSquare);
+				var topSquare = pointer.y > origY ? origY : Math.max(pointer.y,origY-widthSquare);
+				drawingFigure.set({ left: leftSquare, top: topSquare, width: widthSquare, height: widthSquare });
+				break;
+			}
+			case FiguresEnum.PolygonFigure:	
+			{
+				var polygonRadius = Math.sqrt(Math.pow(origX - pointer.x, 2) + Math.pow(origY - pointer.y, 2));
+				var points = DrawingManager.regularPolygonPoints(8,polygonRadius);	
+				drawingFigure.set('points',points);
+				drawingFigure.set({ left: origX-polygonRadius, top: origY-polygonRadius});										
+				break;
+			}
+			case FiguresEnum.TextFigure:
+			{
+				drawingFigure.set({ left: pointer.x, top: pointer.y});										
+				break;
+			}
+		}
 	}
 	
 	static regularPolygonPoints(sideCount,radius)
